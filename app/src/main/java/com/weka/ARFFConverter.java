190cb9eb325315fe86a570cdec982015ff5d218a
pianonerd7 <jxh604@case.edu>
2015-09-30 11:43:16 -0400

    blah

--- app/src/main/java/com/weka/ARFFConverter.java
+++ app/src/main/java/com/weka/ARFFConverter.java
@@ -10,6 +10,9 @@ import android.widget.Button;
 import android.widget.EditText;
 
 import java.io.File;
+import java.util.List;
+
+import csvToArffConverter.*;
 
 public class ARFFConverter extends Activity {
 
@@ -32,8 +35,10 @@ public class ARFFConverter extends Activity {
                         String labelB = labelBFilePath.getText().toString();
                         File fileA = formatFile(labelA);
                         File fileB = formatFile(labelB);
-                        Log.d("toArff", fileA.toString());
-                        Log.d("toArff", fileB.toString());
+                        List<ARFFData> arffDataList = ConvertCSVToARFF.convert(fileA, fileB);
+                        ARFFWriter.convertToARFF(formatFile(getFileName(labelA)+"_train"+".arff"), arffDataList.get(0));
+                        ARFFWriter.convertToARFF(formatFile(getFileName(labelB)+"_test"+".arff"), arffDataList.get(1));
+                        Log.d("ARFFConverter", "MY TAGS ARE ALWAYS FUCKING DECENT. btw success");
                     }
                 }
         );
@@ -44,8 +49,10 @@ public class ARFFConverter extends Activity {
                         String labelB = labelBFilePath.getText().toString();
                         File fileA = formatFile(labelA);
                         File fileB = formatFile(labelB);
-                        Log.d("toArffMeanstd", fileA.toString());
-                        Log.d("toArffMeanstd", fileB.toString());
+                        List<ARFFData> arffDataList = ConvertCSVToARFF.convertWithStat(fileA, fileB);
+                        ARFFWriter.convertToARFF(formatFile(getFileName(labelA+"_train" + ".arff")), arffDataList.get(0));
+                        ARFFWriter.convertToARFF(formatFile(getFileName(labelB+"_test"+ ".arff")), arffDataList.get(1));
+                        Log.d("ARFFConverterMeanStd", "MY TAGS ARE ALWAYS FUCKING DECENT. btw success");
                     }
                 }
         );
@@ -53,8 +60,12 @@ public class ARFFConverter extends Activity {
 
     private File formatFile(String filename){
         String root = Environment.getExternalStorageDirectory().toString();
-        File dir = new File(root+ "/DCIM/");
+        File dir = new File(root+ "/Project 2/");
         dir.mkdir();
         return new File(dir, filename);
+    }
+
+    private String getFileName (String filename){
+        return filename.substring(0, filename.length()-4);
     }
 }
